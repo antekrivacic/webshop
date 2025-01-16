5e7b7ea7e572697232c6ff2153a8e9e6d8446cb2
antekrivi <ante.krivacic@gmail.com>
2025-01-16 17:43:06 +0100

    Improved Login

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,14 +15,20 @@ export class LoginPageComponent implements OnInit{
 
   loginForm!: FormGroup;
   isSubmitted = false;
+  returnUrl = '';
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder,
+     private userService: UserService,
+      private activatedRoute: ActivatedRoute,
+      private router: Router) {}
   
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email:['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
   }
 
   get fc (){
@@ -30,7 +39,9 @@ export class LoginPageComponent implements OnInit{
     this.isSubmitted = true;
     if(this.loginForm.invalid) return;
 
-    alert(`email: ${this.fc.email.value} ,
-      password: ${this.fc.password.value}`);
+    this.userService.login({email:this.fc.email.value,
+       password: this.fc.password.value}).subscribe(() => {
+        this.router.navigateByUrl(this.returnUrl);
+      });
   }
 }
--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
+import { UserService } from '../../../services/user.service';
+import { User } from '../../../shared/models/User';
 
 @Component({
   selector: 'app-header',
@@ -11,10 +13,23 @@ import { CartService } from '../../../services/cart.service';
 export class HeaderComponent {
 
   cartQuantity: number = 0;
+  user!: User
 
-  constructor(cartService : CartService){
+  constructor(cartService : CartService, private userService: UserService){
     cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
     })
+
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+    })
+  }
+
+  logout() :void{
+    this.userService.logout();
+  }
+
+  get isAuth(){
+    return this.user.token;
   }
 }
